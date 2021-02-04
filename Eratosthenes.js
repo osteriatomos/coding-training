86eb5d6f11a8ca131d00748e1df201ff7e5ba798
tksaijo <osteriatomos@gmail.com>
2021-02-05 00:38:48 +0900

    update eratosthenes

--- Eratosthenes.js
+++ Eratosthenes.js
@@ -1,3 +1,13 @@
+N = [
+  [2, 2],
+  [2, 4],
+  [2, 32],
+  [4, 128],
+  [8, 256],
+  [100, 10000],
+  [10000, 1000000],
+];
+
 const getPrime = (num) => {
   let primes = new Array(num + 1).fill(true);
   primes[0] = false;
@@ -13,6 +23,14 @@ const getPrime = (num) => {
   primes.forEach((isPrime, idxNum) => {
     if (isPrime) console.log(idxNum);
   });
+
+  return primes;
 };
 
-getPrime(10);
+const maxNum = Math.max(...N.flat());
+const isPrimeList = getPrime(maxNum);
+
+N.forEach(([start, end]) => {
+  const slicedIsPrimeList = isPrimeList.slice(start, end + 1);
+  console.log(slicedIsPrimeList.filter((isPrime) => isPrime).length);
+});
